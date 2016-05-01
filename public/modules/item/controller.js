7c77314d73851bcd992bfcabad7cfc9886205ddf
Yogesh Sharma <sharma.yogesh.1234@gmail.com>
2016-05-01 12:34:47 -0400

    Updated item

--- public/modules/item/controller.js
+++ public/modules/item/controller.js
@@ -8,6 +8,7 @@ angular.module('ajpmApp').controller('ItemController',
 	['$scope', '$stateParams', 'GetDocumentByIdService',
 	function($scope, $stateParams, GetDocumentByIdService){
 
+	$scope.currentPhoto = 0;
 	GetDocumentByIdService.getDocument('item', $stateParams.itemId, function(doc) {
         $scope.doc = doc;
 		GetDocumentByIdService.getDocument('organization', $scope.doc.manufacturar, function(d1) {
