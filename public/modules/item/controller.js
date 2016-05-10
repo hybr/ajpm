d0847ceeacb3b997bdb12cdef265888e581b6954
sharma.yogsh.1234@gmail.com <sharma.yogsh.1234@gmail.com>
2016-05-10 09:09:17 +0530

    Updated the image slider in item

--- public/modules/item/controller.js
+++ public/modules/item/controller.js
@@ -8,7 +8,6 @@ angular.module('ajpmApp').controller('ItemController',
 	['$scope', '$stateParams', 'GetDocumentByIdService',
 	function($scope, $stateParams, GetDocumentByIdService){
 
-	$scope.currentPhoto = 0;
 	GetDocumentByIdService.getDocument('item', $stateParams.itemId, function(doc) {
         $scope.doc = doc;
 		GetDocumentByIdService.getDocument('organization', $scope.doc.manufacturar, function(d1) {
