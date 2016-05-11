fea888fc1a51b6620ade8a794f51d5eb8b0d5051
Dipanshu <me@dipanshu.online>
2016-05-11 20:11:11 +0530

    analytics

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@
  * This is the entry file for JPM framework.
  * A web framework based on jquery, php, mongodb
  * Adding AngularJS use from this release
- * 
- * This framework is designed to host multiple websites from a single 
- * location and their data in mongodb. These small websites could be 
+ *
+ * This framework is designed to host multiple websites from a single
+ * location and their data in mongodb. These small websites could be
  * personal of commercial websites.
  *
  * LICENSE: Owned by HYBR
@@ -35,7 +35,7 @@ session_start ();
  * Initialize the session array so every time we rebuild it
  */
 /*
- * 
+ *
 foreach($_SESSION as $key) {
 	if (in_array($key, array('user', 'person', 'login_person_id'))) {
 		continue;
@@ -89,5 +89,14 @@ if ($_SESSION['request_type'] == 'service') {
 }
 debugPrintArray($_SESSION, 'SESSION');
 
-
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+	echo "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-77504692-1', 'auto');
+  ga('send', 'pageview');
+		</script>";
+}
 ?>
