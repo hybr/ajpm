c4af1d06524c02a53462f812f4e938f9fcc9ea52
Yogesh Sharma <sharma.yogesh.1234@gmail.com>
2016-05-22 14:17:14 -0400

    the print_directive html files

---
+++ public/modules/my_account/controller.js
@@ -0,0 +1,31 @@
+
+/**
+ * @controller ContactUsController to maintain contact information
+ */
+
+'use strict';
+	
+angular.module('ajpmApp').controller('PersonController', 
+		['$scope', '$rootScope', '$http', 'GetCollectionService',
+		function($scope, $rootScope, $http, GetCollectionService){
+			
+		
+		GetCollectionService.getCollection('person', function(d1) {
+			$scope.docs = d1;
+		});
+
+} ]);
+
+angular.module('ajpmApp').directive("printPerson", function() {
+	var d = {};
+
+	d.restrict = 'E'; 
+
+	d.templateUrl = '/modules/my_account/print_person.html';
+
+	d.scope = {
+		paramPerson : "=argPerson"
+	};
+
+	return d;
+});
