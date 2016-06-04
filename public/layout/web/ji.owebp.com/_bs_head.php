b51d0e42869677f56ba4c27815bbe333b3ff313c
sharma.yogsh.1234@gmail.com <sharma.yogsh.1234@gmail.com>
2016-06-04 23:34:51 +0530

    Added social sites to ji.owebp.com

--- public/layout/web/ji.owebp.com/_bs_head.php
+++ public/layout/web/ji.owebp.com/_bs_head.php
@@ -1,6 +1,23 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="
+Invest in Agriculture and commercial Land in Rajasthan,
+Real Estate Investment In India,
+Invest in Agriculture Land in Rajasthan,
+Invest in Commercial Land in Rajasthan,
+JDA Approved property in jaipur,
+Jaipur investor,
+JDA Approved Property in Jagatpura,
+Buy Agriculture land in jaipur,
+Buy JDA approved property in jaipur,
+Buy commercial land in jaipur,
+Best real estate agent in jaipur,
+best property dealer in jagatpura and ring road,
+Best real estate agent in jagatpura,
+Best real estate agent in  agricultural land in tonk road,
+Best real estate agent in  agricultural land in delhi road
+">
 
 <title><?php
 	/* the default title is Organization is based on organization detail and 
